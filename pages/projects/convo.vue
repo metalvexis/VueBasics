b671bbc00b3cd982827353abb0885c45724fed2d
James Saballegue <jp.saballegue@gmail.com>
2024-01-14 00:25:15 +0800

    update projects

--- pages/projects/convo.vue
+++ pages/projects/convo.vue
@@ -1,24 +1,117 @@
 <template>
-  <UContainer class="flex flex-col md:flex-row p-0 gap-1 sm:gap-2 sm:py-4 sm:px-4 relative h-dvh md:h-auto"
-    style="outline: red solid 1px;">
-    <div class="basis-1/3 flex flex-col border-solid rounded-md border-2">
+  <UContainer
+    class="flex flex-col items-stretch gap-1 sm:gap-2 py-6 relative sm:max-h-[500px] text-center text-riptide-600 dark:text-riptide-300">
+    <h1 class="font-serif text-2xl sm:text-6xl">Convo</h1>
+    <p class="font-sans text-lg sm:text-xl">
+      Chat with everyone visiting the site!
+    </p>
+  </UContainer>
+
+  <UContainer
+    class="flex flex-col sm:flex-row items-stretch px-0 py-2 gap-1 sm:gap-2 sm:py-4 sm:px-4 relative sm:max-h-[500px]">
+    <div class="min-h-0 sm:hidden flex flex-col border-solid rounded-md border-2 border-riptide-500">
       <!-- Convo sessions list -->
+      <UAccordion :items="convosMobileAccordion" :ui="{ wrapper: 'flex flex-col w-full' }">
+
+        <template #default="{ item, index, open }">
+          <UButton variant="ghost">
+            <template #leading>
+              <UIcon :name="item.icon" class="text-xl" />
+              <p>
+
+                {{ item.label }}
+              </p>
+            </template>
+
+            <template #trailing>
+              <UIcon name="i-bi-chevron-down" class="w-5 h-5 ms-auto transform transition-transform duration-200"
+                :class="[open && 'rotate-180']" />
+            </template>
+          </UButton>
+        </template>
+
+
+        <template #convosMobile>
+
+          <ul class="max-h-[200px] overflow-auto">
+            <li v-for="convo in convosList" class="px-4 py-2">
+              <UButton variant="ghost" class="w-full">
+                {{ convo.text }}
+              </UButton>
+            </li>
+          </ul>
+
+        </template>
+      </UAccordion>
     </div>
-    <div class="basis-2/3 flex flex-col gap-1 sm:gap-2">
-      <div class="h-40 sm:h-60 md:h-96 border-solid rounded-md border-2">
+
+    <div class="basis-1/3 hidden sm:flex flex-col border-solid rounded-md border-2 border-riptide-500 overflow-auto">
+      <!-- Convo sessions list -->
+      <p class="font-sans px-4 py-2 text-xl text-riptide-500">Rooms</p>
+      <div class="w-full overflow-auto">
+        <ul>
+          <li v-for="convo in convosList" class="px-4 py-2">
+            <UButton variant="ghost" class="w-full">
+              {{ convo.text }}
+            </UButton>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="h-min basis-2/3 flex flex-col gap-1 sm:gap-2">
+      <div class="h-64 sm:h-80 md:h-96 border-solid rounded-md border-2 border-riptide-500">
         <!-- Convo -->
       </div>
 
-      <div>
-        <UTextarea v-model="convoInput" color="primary" variant="outline" placeholder="Say Hello to the lil elves :D" />
+      <div class="flex flex-row border-riptide-500 border-2 rounded ">
+        <UTextarea class="flex-grow border-riptide-200" v-model="convoInput" variant="none"
+          placeholder="Say Hello to the lil elves :D" />
+        <UDivider class="my-2 w-0 border-solid border-x-[1px] border-riptide-500 justify-self-stretch" />
+        <UButton class="flex-grow-0 border-0 px-4" variant="link" aria-label="Send Message" @click="sendMessage">
+          <UIcon v-if="!buttonClicked" class="text-lg sm:text-xl" name="i-bi-send-fill" />
+          <UIcon v-if="buttonClicked" class="text-lg sm:text-xl" name="i-bi-send" />
+        </UButton>
       </div>
     </div>
   </UContainer>
 </template>
 
 <script lang="ts" setup>
+import type { AccordionItem } from '@nuxt/ui/dist/runtime/types';
+const convosMobileAccordion: AccordionItem[] = [
+  {
+    label: 'Convo Session ID ####',
+    icon: 'i-bi-chat-dots-fill',
+    defaultOpen: false,
+    slot: 'convosMobile',
+  }
+];
+
+const convosList = ref<Record<string, string>[]>([]);
+
+for (let index = 0; index < 5; index++) {
+  convosList.value.push({
+    text: `Convo Session ID ${index}`,
+  });
+}
 
 const convoInput = ref('');
 
+const buttonClicked = ref(false);
+
+watch(buttonClicked, (value) => {
+  if (value) {
+    setTimeout(() => {
+      buttonClicked.value = false;
+    }, 150);
+  }
+})
+
+function sendMessage() {
+  console.log('send message');
+  buttonClicked.value = true;
+  convoInput.value = '';
+}
 
 </script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,15 +1,47 @@
 <template>
   <UContainer class="flex flex-col gap-8 py-6 relative">
-    <h1 class="font-serif font-bold text-6xl text-riptide-600 dark:text-riptide">Projects</h1>
-    <ul>
-      <li>
-        <NuxtLink to="/projects/convo">Convo</NuxtLink>
-      </li>
-    </ul>
+    <h1 class="font-serif text-2xl sm:text-6xl text-riptide-600 dark:text-riptide">Projects</h1>
+
+
+    <div class="px-8 py-4 flex justify-start">
+      <NuxtLink v-for="g of gallery" :to="g.link">
+        <UCard :ui="{ background: 'red dark:white' }">
+          <template #header>
+            <h2 class="text-center font-serif text-xl sm:text-2xl text-riptide-600 dark:text-riptide">
+              {{ g.title }}
+            </h2>
+            <p class="text-center font-sans sm:text-lg text-riptide-600 dark:text-riptide">
+              {{ g.description }}
+            </p>
+          </template>
+
+          <img class="aspect-square object-cover" :src="g.image" alt="Photo of Convo App">
+        </UCard>
+      </NuxtLink>
+
+    </div>
+
   </UContainer>
-  <NuxtPage :page-key="route => route.fullPath" />
+  <!-- <NuxtPage :page-key="route => route.fullPath" /> -->
 </template>
 
 <script lang="ts" setup>
+type GalleryItem = {
+  title: string
+  description: string
+  image: string
+  link: string
+};
+
+const gallery: GalleryItem[] = [
+  {
+    title: 'Convo',
+    image: '/screenshots/ss_convo.png',
+    link: '/projects/convo',
+    description: 'Chat with everyone visiting the site!',
+  }
+]
+
+
 
 </script>
